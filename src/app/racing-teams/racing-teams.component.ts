0194074939e30910df5ed3231821b082b8e5326f
Leon Timmerman <leontimmerman123@gmail.com>
2018-11-19 13:11:07 +0100

    Added delete methods for racingTeams + drivers

--- src/app/racing-teams/racing-teams.component.ts
+++ src/app/racing-teams/racing-teams.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RacingTeamsService, Team } from '../racing-teams.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-racingTeams',
@@ -10,22 +11,38 @@ export class RacingTeamsComponent implements OnInit {
   racingTeams: Team[];
   displayedColumns: string[] = ['name', 'salary', 'buttons'];
 
-  constructor(private racingTeamsService: RacingTeamsService) { }
+  constructor(private racingTeamsService: RacingTeamsService, private router: Router) { }
 
-  ngOnInit() {
+  fetchData() {
     this.racingTeamsService.getRacingTeams()
       .subscribe((teams: Team[]) => {
         this.racingTeams = teams;
       });
   }
 
-  deleteRacingTeam(teamid: number){
-    this.racingTeamsService.deleteRacingTeam(teamid)
-    .subscribe();
+  ngOnInit() {
+    this.fetchData();
   }
 
-  deleteDriver(driverid : number){
+  deleteRacingTeam(teamid: number) {
+    this.racingTeamsService.deleteRacingTeam(teamid)
+      .subscribe(
+        () => {
+          this.fetchData();
+        }, error => {
+          console.log(error.message);
+        }
+      );
+  }
+
+  deleteDriver(driverid: number) {
     this.racingTeamsService.deleteDriver(driverid)
-    .subscribe();
+      .subscribe(
+        () => {
+          this.fetchData();
+        }, error => {
+          console.log(error.message);
+        }
+      );
   }
 }
